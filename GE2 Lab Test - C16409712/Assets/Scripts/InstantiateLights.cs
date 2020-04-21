078953309380f10b867b27e91d29646c6c1f708f
Stephen Wann <c16409712@mydit.ie>
2020-04-21 18:30:16 +0100

    Instantiate Traffic Lights

---
+++ GE2 Lab Test - C16409712/Assets/Scripts/InstantiateLights.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateLights : MonoBehaviour
+{
+    public static int numberOfLights = 10;
+    public GameObject trafficLightPrefab;
+    GameObject[] trafficLight = new GameObject[numberOfLights];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            GameObject instanceLight = (GameObject)Instantiate(trafficLightPrefab);
+            instanceLight.transform.position = this.transform.position;
+            instanceLight.transform.parent = this.transform;
+
+            this.transform.eulerAngles = new Vector3(0, (360 / numberOfLights) * i, 0);
+            instanceLight.transform.position = Vector3.forward * 10;
+            trafficLight[i] = instanceLight;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ GE2 Lab Test - C16409712/Assets/Scripts/TrafficLightColors.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrafficLightColors : MonoBehaviour
+{
+    public Material greenLight;
+    public Material yellowLight;
+    public Material redLight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
