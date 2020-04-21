5600cc9332d3c6a6c081cdf69b75e5e46152a847
Stephen Wann <c16409712@mydit.ie>
2020-04-21 18:53:13 +0100

    Traffic Lights Starting Colors

--- GE2 Lab Test - C16409712/Assets/Scripts/InstantiateLights.cs
+++ GE2 Lab Test - C16409712/Assets/Scripts/InstantiateLights.cs
@@ -8,9 +8,15 @@ public class InstantiateLights : MonoBehaviour
     public GameObject trafficLightPrefab;
     GameObject[] trafficLight = new GameObject[numberOfLights];
 
+    Color[] colors = new Color[3];
+
     // Start is called before the first frame update
     void Start()
     {
+        colors[0] = Color.green;
+        colors[1] = Color.yellow;
+        colors[2] = Color.red;
+
         for (int i = 0; i < 10; i++)
         {
             GameObject instanceLight = (GameObject)Instantiate(trafficLightPrefab);
@@ -20,6 +26,8 @@ public class InstantiateLights : MonoBehaviour
             this.transform.eulerAngles = new Vector3(0, (360 / numberOfLights) * i, 0);
             instanceLight.transform.position = Vector3.forward * 10;
             trafficLight[i] = instanceLight;
+
+            instanceLight.GetComponent<Renderer>().material.color = colors[Random.Range(0, colors.Length)];
         }
     }
 
