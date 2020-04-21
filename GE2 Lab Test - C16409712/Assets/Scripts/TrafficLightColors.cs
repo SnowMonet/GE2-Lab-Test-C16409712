c1c932a4b4c385a890bc9d7eabad344172c1554a
Stephen Wann <c16409712@mydit.ie>
2020-04-21 20:13:14 +0100

    Traffic Lights Color Changing

--- GE2 Lab Test - C16409712/Assets/Scripts/TrafficLightColors.cs
+++ GE2 Lab Test - C16409712/Assets/Scripts/TrafficLightColors.cs
@@ -4,19 +4,57 @@ using UnityEngine;
 
 public class TrafficLightColors : MonoBehaviour
 {
-    public Material greenLight;
-    public Material yellowLight;
-    public Material redLight;
+    /*  
+      public Material greenLight;
+      public Material yellowLight;
+      public Material redLight;
+    */
+
+    Renderer trafficLightRenderer;
+
+    float colorDelay;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        trafficLightRenderer = GetComponent<Renderer>();
+
+        StartCoroutine(ChangeColorLoop());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    IEnumerator ChangeColorLoop()
+    {
+        while(true)
+        {
+            ChangeColor();
+            yield return new WaitForSeconds(colorDelay);
+        }
+    }
+
+    void ChangeColor()
+    {
+        if (trafficLightRenderer.material.color == Color.green)
+        {
+            colorDelay = 4;
+            trafficLightRenderer.material.color = Color.yellow;
+        }
+
+        else if (trafficLightRenderer.material.color == Color.yellow)
+        {
+            colorDelay = Random.Range(5,10);
+            trafficLightRenderer.material.color = Color.red;
+        }
+
+        else if (trafficLightRenderer.material.color == Color.red)
+        {
+            colorDelay = Random.Range(5, 10);
+            trafficLightRenderer.material.color = Color.green;
+        }
     }
 }
